a8d8b140a255221c41b0fccf34087a3e6afe62f1
amang <79791017+MusthofainalAkhyarB@users.noreply.github.com>
2023-12-01 15:46:17 +0700

    ok

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,8 +7,9 @@ class Dashboard extends CI_Controller
     public function index()
     {
         $data['title'] = 'Dashboard';
-        $this->load->view('templates/header');
-        $this->load->view('templates/sidebar');
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
         $this->load->view('dashboard');
         $this->load->view('templates/footer');
     }
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -49,7 +49,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="<?= base_url('siswa') ?>" class="nav-link">
                                 <i class="nav-icon fas fa-users"></i>
                                 <p>Siswa</p>
                             </a>
@@ -74,12 +74,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">Starter Page</h1>
+                            <h1 class="m-0 text-dark"><?= $title ?></h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Starter Page</li>
+                                <li class="breadcrumb-item active"><?= $title ?></li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
